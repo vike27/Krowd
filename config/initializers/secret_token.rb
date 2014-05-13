f0c5e8a3634ebdd7159297ee3ad603901c2d3427
Javier Noris <norisjavier@gmail.com>
2014-05-13 06:10:31 -0700

    first commit set up devise,bootstrap and basic layout

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Krowd::Application.config.secret_key_base = 'cb8b7e1e3a78316de6caf46e08e3506ba9b118ec58840544a3d0fff849f9bda388e2be8aeb4189d1f9502043c890f4b3e1ad6b7d4909007757ec56b34838a57e'
